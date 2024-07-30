aec98955ff5a36d00dd00e000d83adb5eb4085ff
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-07-30 12:25:08 +0600

    Add files via upload

---
+++ Project_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 100; i++) {
+        printf("I want to be the best programmer in the world\n ");
+        Sleep(200);
+    }
+
+    getchar();
+
+    return 0;
+}
+
