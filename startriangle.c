cb69cf7235f6d2b9bd93caa61d9b98d3139a7ea2
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-08-16 22:12:19 +0600

    Add files via upload

---
+++ startriangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+ int m,n;
+ printf("Enter the number of row :");
+ scanf("%d",&m);
+ printf("Enter the number of column :");
+ scanf("%d",&n);
+ for(int i=1;i<=m;i++){
+    for(int j=1;j<=i;j++){
+        printf("*");
+    }
+    printf("\n");
+ }
+
+
+
+
+
+
+
+
+
+
+}
