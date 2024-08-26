b38e3fb0b7ffe213ffea4cd6a2c0994d8650d147
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-08-26 22:08:19 +0600

    Add files via upload

---
+++ alphabet_triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number of rows and column :");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+         int a=65;
+        for(int j=1;j<=i;j++){
+            printf("%c ",a);
+            a++;
+        }
+        printf("\n");
+    }
+}
