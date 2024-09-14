096924e61bc54d5cd1f7c2223ab760c56cd60b57
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-09-14 08:24:16 +0600

    Add files via upload

---
+++ string_basic_(4).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char str[40];
+    // scanf ("%s",str); space can considered
+     gets(str);   /*full sentence can be considered*/
+     printf("Input was : %s",str);
+
+    return 0;
+}
