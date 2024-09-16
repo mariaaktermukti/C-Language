67b29560cce6c427599356c9acfd4eaca75a536c
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-09-16 23:26:35 +0600

    Add files via upload

---
+++ string_basic(11).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+   char *ptr="Mukti's Dream car is Mercedes-Benz.";
+   printf("%s\n", ptr);
+   ptr="Mukti's Dream car is Audi.";
+   printf("%s", ptr);
+    return 0;
+}
