8ec1bc5a910df2cac4e768aa2958f346e8f7fbb6
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-10-11 08:54:42 +0600

    Add files via upload

---
+++ file_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  FILE *file;
+  file = fopen("test.txt","w");
+  if(file==NULL){
+    printf("File does not exist");
+  }
+    else{
+       printf("File is opened");
+       fclose(file);
+    }
+
+return 0;
+}
